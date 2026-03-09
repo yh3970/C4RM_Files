1dad709ff38e86bce4d4ed129877a153383fa9cc
yh3970 <yh3970@columbia.edu>
2026-03-09 19:02:15 -0400

    Update VaR_File.py

--- Python_HW3_StarterFiles/VaR_File.py
+++ Python_HW3_StarterFiles/VaR_File.py
@@ -2,9 +2,11 @@ import numpy as np
 
 def VaR(r, confidence, principal=1):
     # This function returns the left tail value
-    # confidence = certaint that losses are not greater than the VaR value
+    # confidence = certain that losses are not greater than the VaR value
     # r = an array of stock returns
     # out = positively stated value of r at the 1-confidence percentile * principal
 
-    out = .01 # Calculate the percentile
-    return out  # Return the absolute value of the calculated percentile
+    percentile_value = np.percentile(r, (1 - confidence) * 100)
+    out = abs(percentile_value) * principal
+
+    return out
